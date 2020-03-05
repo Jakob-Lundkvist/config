f72f12bcfb921cbf489d4fd24c395fca35b7f514
Jakob-Lundkvist <jakoblundkvist3@gmail.com>
2020-03-05 08:29:05 +0100

    ssh added

--- setup.sh
+++ setup.sh
@@ -15,4 +15,4 @@ cp init.vim ~/.config/nvim/
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-
+ssh-keygen
